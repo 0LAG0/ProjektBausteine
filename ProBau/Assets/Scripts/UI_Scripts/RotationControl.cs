5dcf68c81f0de855129d5240ba8eeb1cd955a941
Vanessa <s0561035@htw-berlin.de>
2019-05-21 16:50:23 +0200

    Added zoom and rotation function
    Added 2 scripts
    - RotationControl on 3D model GameObject (to rotate it with mouse drag)
    - ZoomControl on camera (to zoom in / out with mouse scroll wheel)

---
+++ ProBau/Assets/Scripts/UI_Scripts/RotationControl.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationControl : MonoBehaviour
+{
+    float rotationSpeed = 2f;
+
+    void OnMouseDrag()
+    {
+        float xAxisRotation = Input.GetAxis("Mouse X") * rotationSpeed;
+        float yAxisRotation = Input.GetAxis("Mouse Y") * rotationSpeed;
+
+        transform.Rotate(Vector3.down, xAxisRotation);
+        transform.Rotate(Vector3.right, yAxisRotation);
+    }
+}
---
+++ ProBau/Assets/Scripts/UI_Scripts/ZoomControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZoomControl : MonoBehaviour
+{
+    float MouseZoomSpeed = 15.0f;
+    Camera cam;
+
+    void Start()
+    {
+        cam = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        float scroll = Input.GetAxis("Mouse Scrollwheel");
+        cam.fieldOfView -= scroll * MouseZoomSpeed;
+    }
+}
