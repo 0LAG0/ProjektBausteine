f84fa46cb2de3ca22f23089dd6341157ee875169
Tatjana <tatjana-kukic@hotmail.de>
2019-05-22 09:09:59 +0200

    Models, Script
    added 2 preselected models, added a subfolder, model selection script kind of working (still working on it)

--- ProBau/Assets/Scripts/UI_Scripts/ChangeModell.cs
+++ ProBau/Assets/Scripts/UI_Scripts/ChangeModell.cs
@@ -2,17 +2,57 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/* Sources: 
+ * https://www.youtube.com/watch?v=T-AbCUuLViA&feature=youtu.be 
+ * Character Selection [Tutorial][C#] - Unity 3d 
+ * https://docs.unity3d.com/ScriptReference/UI.Dropdown-value.html */
+
 public class ChangeModell : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private List<GameObject> models;
+    // Default index of the model
+    private int selectionIndex = 0;
+
+    private void Start()
     {
-        
+        models = new List<GameObject>();
+
+        foreach(Transform t in transform)
+        {
+            models.Add(t.gameObject); 
+            t.gameObject.SetActive(false);
+        }
+
+        models[selectionIndex].SetActive(true);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update() // ändern für Dropdown!
     {
-        
+        if (Input.GetKeyDown(KeyCode.A))
+            Select(2);
+        if (Input.GetKeyDown(KeyCode.S))
+            Select(0);
+        if (Input.GetKeyDown(KeyCode.D))
+            Select(1);
+
     }
+
+    // Select any model at any time
+    public void Select(int index)
+    {
+        if (index == selectionIndex)
+        {
+            return;
+        }
+
+        if (index < 0 || index >= models.Count)
+        {
+            return;
+        }
+
+        models[selectionIndex].SetActive(false);
+        selectionIndex = index;
+        models[selectionIndex].SetActive(true);
+    }
+
 }
