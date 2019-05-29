d02f90235b4a8abb8c48cee4df2328340df95451
Tatjana <tatjana-kukic@hotmail.de>
2019-05-29 14:18:25 +0200

    changed position models, fileexplorer not functioning, changed color background

---
+++ ProBau/Assets/Scripts/UI_Scripts/FileExplorer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+/* Source:
+ * https://docs.unity3d.com/ScriptReference/EditorUtility.OpenFilePanel.html
+ */
+
+public class FileExplorer : EditorWindow {
+
+    [MenuItem("Example/Overwrite Texture")]
+
+    public static void Apply()
+    {
+        Texture2D texture = Selection.activeObject as Texture2D;
+
+        if (texture == null)
+        {
+            EditorUtility.DisplayDialog("Select Texture", "You must select a texture first!", "OK");
+            return;
+        }
+
+        if (path.Length != 0)
+        {
+            var fileContent = File.ReadAllBytes(path);
+            texture.LoadImage(fileContent);
+        }
+    }
+}
