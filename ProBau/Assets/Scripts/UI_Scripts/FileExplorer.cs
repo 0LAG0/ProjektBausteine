f3b6123433f802fd108e7924747c8079e26184f2
Tatjana <tatjana-kukic@hotmail.de>
2019-05-29 21:51:16 +0200

    changed backgroundcolor, deleted fileexplorer.cs content

--- ProBau/Assets/Scripts/UI_Scripts/FileExplorer.cs
+++ ProBau/Assets/Scripts/UI_Scripts/FileExplorer.cs
@@ -6,26 +6,5 @@ using System.IO;
 
 /* Source:
  * https://docs.unity3d.com/ScriptReference/EditorUtility.OpenFilePanel.html
+ * https://docs.unity3d.com/ScriptReference/EditorUtility.OpenFolderPanel.html
  */
-
-public class FileExplorer : EditorWindow {
-
-    [MenuItem("Example/Overwrite Texture")]
-
-    public static void Apply()
-    {
-        Texture2D texture = Selection.activeObject as Texture2D;
-
-        if (texture == null)
-        {
-            EditorUtility.DisplayDialog("Select Texture", "You must select a texture first!", "OK");
-            return;
-        }
-
-        if (path.Length != 0)
-        {
-            var fileContent = File.ReadAllBytes(path);
-            texture.LoadImage(fileContent);
-        }
-    }
-}
