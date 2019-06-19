034dc38e68f30c8287181464c17498afb2b8f8f8
Tatjana <tatjana-kukic@hotmail.de>
2019-06-19 23:53:10 +0200

    working on open anleitung

---
+++ ProBau/Assets/Scripts/UI_Scripts/OpenManual.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* Sources
+ * Unity Tutorial: Open Panel on Button Click
+ * https://www.youtube.com/watch?v=LziIlLB2Kt4&t=120s
+ * Unity3D GUI 10: Windows (draggable)
+ * https://www.youtube.com/watch?v=5VcARDodbb0 */
+
+
+public class OpenManual : MonoBehaviour
+{
+    public GameObject Panel;
+
+    void OpenPanel()
+    {
+        if (Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+}
