25690455cfbfca1b39ad0b59c024ec4beb112418
Tatjana <tatjana-kukic@hotmail.de>
2019-06-19 15:31:01 +0200

    Working on Texturauswahl
    Textur kinda working, Verbindung zu Dropdown fehlt noch

--- ProBau/Assets/Scripts/UI_Scripts/ChangeTexture.cs
+++ ProBau/Assets/Scripts/UI_Scripts/ChangeTexture.cs
@@ -23,7 +23,7 @@ public class ChangeTexture : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.A))
         {
             currentTexture++;
             currentTexture %= textures.Length;
