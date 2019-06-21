805a6688e101027f3d69b8dd7be938b385584f40
Tatjana <tatjana-kukic@hotmail.de>
2019-06-21 16:32:06 +0200

    small changes

--- ProBau/Assets/Scripts/UI_Scripts/ChangeTexture.cs
+++ ProBau/Assets/Scripts/UI_Scripts/ChangeTexture.cs
@@ -20,24 +20,35 @@ public class ChangeTexture : MonoBehaviour
     {
         switch(myDropdown.value)
         {
+            // keine Textur, Standard
+            case 0:
+                GetComponent<Renderer>().material.mainTexture = textures[0];
+                break;
+            // Textur Zement
             case 1:
                 GetComponent<Renderer>().material.mainTexture = textures[1];
                 break;
+            // Textur Keramik
             case 2:
                 GetComponent<Renderer>().material.mainTexture = textures[2];
                 break;
+            // Textur HTW Gruen
             case 3:
                 GetComponent<Renderer>().material.mainTexture = textures[3];
                 break;
+            // Textur Leopard
             case 4:
                 GetComponent<Renderer>().material.mainTexture = textures[4];
                 break;
+            // Textur Gemaelde
             case 5:
                 GetComponent<Renderer>().material.mainTexture = textures[5];
                 break;
+            // Textur Himmel
             case 6:
                 GetComponent<Renderer>().material.mainTexture = textures[6];
                 break;
+            // Textur Holz
             case 7:
                 GetComponent<Renderer>().material.mainTexture = textures[7];
                 break;
