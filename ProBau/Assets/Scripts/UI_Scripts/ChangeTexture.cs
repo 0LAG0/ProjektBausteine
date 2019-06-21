57c7de55cfd0fbc36cb5dab23b6f0e41ec0fa8d5
Tatjana <tatjana-kukic@hotmail.de>
2019-06-21 15:11:00 +0200

    TextureDropdown works, changed layout of animation window

--- ProBau/Assets/Scripts/UI_Scripts/ChangeTexture.cs
+++ ProBau/Assets/Scripts/UI_Scripts/ChangeTexture.cs
@@ -6,32 +6,42 @@ using UnityEngine.UI;
 /* Source: 
  * Unity3D Tutorial - Texture Swap
  * https://www.youtube.com/watch?v=aLOru6QQShM 
- * How to Assign Materials Using C# Code
- * https://www.youtube.com/watch?v=AAc_R9bp6zI */
+ * Unity 5 C# Using Dropdown menu to change background color with switch Statement
+ * https://www.youtube.com/watch?v=6qQ80N3vRQU */
 
 public class ChangeTexture : MonoBehaviour
 
 {
-    // Attach this script to a DropDown GameObject
-    // Dropdown m_Dropdown;
     
     public Texture[] textures;
-    public int currentTexture;
-
+    public Dropdown myDropdown;
 
     public void Update()
     {
-
-        // Fetch the DropDown component from the GameObject
-        //m_Dropdown = GetComponent<Dropdown>();
-
-        if (Input.GetKeyDown(KeyCode.A))
+        switch(myDropdown.value)
         {
-            currentTexture++;
-            currentTexture %= textures.Length;
-            GetComponent<Renderer>().material.mainTexture = textures[currentTexture];
+            case 1:
+                GetComponent<Renderer>().material.mainTexture = textures[1];
+                break;
+            case 2:
+                GetComponent<Renderer>().material.mainTexture = textures[2];
+                break;
+            case 3:
+                GetComponent<Renderer>().material.mainTexture = textures[3];
+                break;
+            case 4:
+                GetComponent<Renderer>().material.mainTexture = textures[4];
+                break;
+            case 5:
+                GetComponent<Renderer>().material.mainTexture = textures[5];
+                break;
+            case 6:
+                GetComponent<Renderer>().material.mainTexture = textures[6];
+                break;
+            case 7:
+                GetComponent<Renderer>().material.mainTexture = textures[7];
+                break;
         }
-
     }
 
 }
