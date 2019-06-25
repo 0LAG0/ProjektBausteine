a528e675170d5920dc45bb6151f17e8a260ff151
Xiunu <jan@senkowski-engelhardt.de>
2019-06-25 18:53:28 +0200

    Finalized merge and added a faster way to assign textures

---
+++ ProBau/Assets/Scripts/UI_Scripts/TextureController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+[RequireComponent(typeof(Dropdown))]
+public class TextureController : MonoBehaviour
+{
+    public ChangeModel modelSelector;
+
+    public List<NamedTexture> namedTextures;
+    private Dropdown dropdown;
+
+    public Texture activeTexture;
+    private int currentIndex = -1;
+
+    private void Awake()
+    {
+        dropdown = this.GetComponent<Dropdown>();
+        dropdown.ClearOptions();
+        List<string> optionNames = new List<string>();
+        namedTextures.ForEach(nt => optionNames.Add(nt.name));
+        dropdown.AddOptions(optionNames);
+        Select(0);
+    }
+
+    public void Select(int index)
+    {
+        if (index == currentIndex || index < 0 || index >= namedTextures.Count)
+        {
+            return;
+        }
+
+        currentIndex = index;
+        activeTexture = namedTextures[currentIndex].texture;
+
+        if (modelSelector.activeObject != null)
+        {
+            modelSelector.activeObject.GetComponentInChildren<Renderer>().material.mainTexture = activeTexture;
+        }
+    }
+}
