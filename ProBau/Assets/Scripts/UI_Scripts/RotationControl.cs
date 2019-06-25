eadb825329b6f5e9f7b1ee4a2910ee4bb06a7ef1
Vanessa <v.meibaum@web.de>
2019-06-25 21:58:41 +0200

    Update RotationControl.cs
    just a typo

--- ProBau/Assets/Scripts/UI_Scripts/RotationControl.cs
+++ ProBau/Assets/Scripts/UI_Scripts/RotationControl.cs
@@ -27,7 +27,7 @@ public class RotationControl : MonoBehaviour
         Vector3 mousePos = Input.mousePosition;
         Vector2 normalizedMousePos = new Vector2(mousePos.x / Screen.width, mousePos.y / Screen.height);
 
-        // check if there is a left mouse button as input and if mouse in within preview area
+        // check if there is a left mouse button as input and if mouse is within preview area
         if (!Input.GetMouseButton(0)
             || normalizedMousePos.x < previewRectTransform.anchorMin.x
             || normalizedMousePos.x > previewRectTransform.anchorMax.x
