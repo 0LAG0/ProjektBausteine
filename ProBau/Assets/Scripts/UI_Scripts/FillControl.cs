956da570c41451fd2289765fbd9f65cafd4bf5ec
Vanessa <s0561035@htw-berlin.de>
2019-06-26 15:00:30 +0200

    Changed filling toggle to slider

---
+++ ProBau/Assets/Scripts/UI_Scripts/FillControl.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class FillControl : MonoBehaviour
+{
+    Text fillText;
+
+    public void Start()
+    {
+        fillText = GameObject.Find("Text_Fill-Thickness").GetComponent<Text>();
+        setText(GetComponent<Slider>().value);
+    }
+
+    public void changeHeight()
+    {
+        setText(GetComponent<Slider>().value);
+    }
+
+    private void setText(float height)
+    {
+        fillText.text = "Wandstärke: " + (Mathf.Round(height) + 1) + " cm";
+    }
+}
